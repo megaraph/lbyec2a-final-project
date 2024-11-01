75e20a4825c8c173661cc5162014f7518d3f94c9
megaraph <47447126+megaraph@users.noreply.github.com>
2024-11-01 13:05:08 +0800

    add new config file

---
+++ src/config.h
@@ -0,0 +1,12 @@
+// menu.h
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// MENU
+#define ITEMS 20 // number of menu items
+
+// USER DATABASE
+#define ENCRYPT_SIZE 64 // max size of username and password
+#define DB_SIZE 3       // number of users in database
+
+#endif // CONFIG_H
