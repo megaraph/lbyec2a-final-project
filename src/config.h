5e98fa4fcf57630014f58212439b0ae18f734655
megaraph <47447126+megaraph@users.noreply.github.com>
2024-11-02 03:02:31 +0800

    update menu content and items

--- src/config.h
+++ src/config.h
@@ -3,7 +3,7 @@
 #define CONFIG_H
 
 // MENU
-#define ITEMS 20 // number of menu items
+#define ITEMS 30 // number of menu items
 
 // USER DATABASE
 #define ENCRYPT_SIZE 64 // max size of username and password
