5e35a5e02e5ae37b083c6e676ff0b4ae4f784060
megaraph <47447126+megaraph@users.noreply.github.com>
2024-11-01 13:05:20 +0800

    add new database file

---
+++ src/database.h
@@ -0,0 +1,13 @@
+// database.h
+#ifndef DATABASE_H
+#define DATABASE_H
+
+#include "config.h"
+
+static const char DATABASE[DB_SIZE][2][ENCRYPT_SIZE] = {
+    {"raph", "123"},
+    {"abc", "324"},
+    {"hjk", "567"},
+};
+
+#endif // DATABASE_H
